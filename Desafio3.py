d5cc9d8a03891a835a55b0e44f531d376ddd2185
Bruno <bertinmarquez84@gmail.com>
2023-05-03 22:39:29 -0300

    atz

--- Desafio3.py
+++ Desafio3.py
@@ -1,7 +1,5 @@
-# Desafio Interagir com usuário pedidndo dois numeros, somando e exibindo o resultado na tela
-primeiro = input ("Primeiro Número = " )
-segundo = input ("Segundo Número = ")
-print ("A soma é", int(primeiro)+ int(segundo))# Neste caso a concatenação de string e numeros (inteiros) é escrita desta forma
-
-
-
+# Desafio Interagir com usuário pedidindo dois numeros, somando e exibindo o resultado na tela
+primeiro = input("Primeiro Número = ")
+segundo = input("Segundo Número = ")
+print("A soma é",
+      int(primeiro) + int(segundo))  # Neste caso a concatenação de string e numeros (inteiros) é escrita desta forma
