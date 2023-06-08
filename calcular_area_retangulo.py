9628df84ffb9a0e47050b03de1d187722f9a7ea0
Bruno Bertin Marquez <86536183+Marquezbertin@users.noreply.github.com>
2023-06-07 22:32:03 -0300

    area do retangulo

---
+++ calcular_area_retangulo.py
@@ -0,0 +1,7 @@
+# Escreva um algoritmo para calcular a área de um retângulo
+# Formula A=b⋅h, onde A = Area , b = Base (do retangulo), h = Altura (do retangulo)
+nome = input("Por favor insira seu nome: ")
+print ("Obrigado", nome, "!")
+b = input ("Por favor informe a medida em centimetros, da base do retangulo:  ")
+a = input  ("Por favor informe a medida em centimetros, da altura do retangulo:  ")
+print (nome, "a area do retangulo em questão é de: " , float(b)*float(a), "cm " ) 
